079e0a5121b8f88bd4e532a0b6e71c197cfaa31f
eightynine77 <175763558+eightynine77@users.noreply.github.com>
2026-07-09 18:49:08 +0700

    Update pass-search.sh

--- pass-search.sh
+++ pass-search.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# this script only works on bitwarden json. if you want to make this script work for json exported from other
+# password manager, then modify the jq line
+
 # Hardcoded JSON file
-JSON_FILE="your_password_directory_here.json"  # Change this to your actual file name
+JSON_FILE="/path/to/your/bitwarden/exported/json/bitwarden.json"  # Change this to the path of your exported bitwarden password
 
 # Check if user provided search value
 if [ -z "$1" ]; then
-    cat mypass
+    cat $JSON_FILE
+    echo ""
+    echo ""
+    echo ""
+    echo "=================="
+    echo "Usage: pass-search.sh <search_value>"
+    echo ""
+    echo "example: pass-search.sh youtube account"
+    exit 1
 fi
 
 SEARCH_VALUE="$1"
@@ -19,12 +30,11 @@ fi
 echo "Searching for value: '$SEARCH_VALUE' in $JSON_FILE..."
 
 jq --arg value "$SEARCH_VALUE" '
-    walk(
-        if type == "object" or type == "array" then
-            if tostring | test($value) then .
-            else empty
-            end
-        else .
-        end
+  .items[]
+  | select(
+      any(
+        .. | strings;
+        ascii_downcase | contains($value | ascii_downcase)
+      )
     )
 ' "$JSON_FILE"
