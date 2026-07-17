00cd4d98557c8240167252a68e930add388337dc
eightynine77 <175763558+eightynine77@users.noreply.github.com>
2026-07-17 11:37:24 +0700

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
+# ========================
+# this bash script is for searching files and folders
 
-if [ $# -eq 0 ]; then
-    echo "Usage: search.sh <search query>"
-    echo ""
-    echo "for example: search.sh the walking cat"
+LOCATION="/"
+
+OPTIONS=$(getopt -o l: --long location: -- "$@")
+if [ $? -ne 0 ]; then
     exit 1
 fi
 
-sudo find / -xdev -iname "*$*" -print 2>/dev/null
+eval set -- "$OPTIONS"
+
+while true; do
+    case "$1" in
+        -l|--location)
+            LOCATION="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+if [ $# -eq 0 ]; then
+    echo "Usage: search.sh [--location <directory>|-l <directory>] <search term>"
+    echo "search.sh <search term>"
+    echo ""
+    echo "example: search.sh -l /home/user/Downloads readme.txt"
+    echo 'search.sh -l "/home/user/camera files" selfie.png'
+    echo "default search: search.sh program files"
+    exit 1
+fi
+
+SEARCH_TERM="$*"
+
+sudo find "$LOCATION" -xdev -iname "*$SEARCH_TERM*" -print 2>/dev/null
